6272631018aecd7506c6790bc46709636f9de569
SeoulBhin <ahrrhks147@gmail.com>
2025-09-29 01:36:05 +0900

    test

--- backend/src/main/java/com/example/backend/controller/ApiController.java
+++ backend/src/main/java/com/example/backend/controller/ApiController.java
@@ -1,9 +1,15 @@
+package com.example.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
     @GetMapping("/test")
     public String test() {
-        return "This is a test API endpoint";
+        return "API Test OK!";
     }
 }
