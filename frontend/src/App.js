a3ec7bfac0b5e7a14dc460435333341b459d4e33
SeoulBhin <ahrrhks147@gmail.com>
2025-09-28 18:52:48 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import axios from 'axios'; // axios 라이브러리 사용
 
 function App() {
-  // test 
+  // test build
   const [message, setMessage] = useState('Loading...');
 
   useEffect(() => {
