37de946e5c81ed57177c694c363318f15f6cec03
di593488-sys <di593488@gmail.com>
2025-12-05 20:30:21 +0900

    Bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./front/Layout";
+import Layout from "./front/layout";
 
 import HomePage from "./front/HomePage";
 import IntroPage from "./front/IntroPage";
