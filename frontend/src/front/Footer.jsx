3c0963b87e9c8d3f6adb6af33ae1d234e85aafae
개그튼코딩 <hoon62261662@gmail.com>
2025-12-10 13:17:35 +0900

    Create Footer.jsx
    밑바닥

---
+++ frontend/src/front/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="app-footer">
+            <p>© 2025 Watchout. All rights reserved.</p>
+            <p>개인정보 처리방침 | 이용 약관</p>
+        </footer>
+    );
+}
