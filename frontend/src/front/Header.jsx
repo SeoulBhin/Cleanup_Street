345511f00f3a15772d44b0ec6024bb302d071338
개그튼코딩 <hoon62261662@gmail.com>
2025-12-10 12:54:56 +0900

    Update Header.jsx

--- frontend/src/front/Header.jsx
+++ frontend/src/front/Header.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import { useAuthModal } from '../contexts/AuthModalContext'; 
 
-export default function Header({ onLogoClick }) {
-    const { 
-        isLoggedIn, 
-        userInfo, 
-        handleLogout, 
-        openLoginModal, 
-        openSignupModal 
-    } = useAuthModal();
-    
-    
-    // 로그인 전 UI (폼 + 로그인/회원가입 버튼)
-    const LoggedOutView = () => (
+function LoggedOutView({ openLoginModal, openSignupModal }) {
+    return (
         <>
             <button className="btn btn-login" onClick={openLoginModal}>
                 로그인
@@ -22,28 +12,47 @@ export default function Header({ onLogoClick }) {
             </button>
         </>
     );
+}
 
-    // 로그인 후 UI (환영 메시지 + 로그아웃 버튼)
-    const LoggedInView = () => (
+function LoggedInView({ userInfo, handleLogout }) {
+    return (
         <>
             <span className="welcome-message">
-                {userInfo?.username ? `${userInfo.username}님 반갑습니다!` : '사용자님 반갑습니다!'}
+                {userInfo?.username
+                    ? `${userInfo.username}님 반갑습니다!`
+                    : '사용자님 반갑습니다!'}
             </span>
             <button className="btn btn-login" onClick={handleLogout}>
                 로그아웃
             </button>
         </>
     );
+}
 
+export default function Header({ onLogoClick }) {
+
+    // Context 사용
+    const { 
+        isLoggedIn, 
+        userInfo, 
+        handleLogout, 
+        openLoginModal, 
+        openSignupModal 
+    } = useAuthModal();
 
     return (
         <header className="app-header">
-            <h1 onClick={onLogoClick} style={{ userSelect: "none" }}>
+            <h1 
+                onClick={onLogoClick} 
+                style={{ userSelect: "none", cursor: "pointer" }}
+            >
                 와챠우! (Watch out!)
             </h1>
+
             <div className="login-section">
-                {/* Context 상태에 따른 조건부 렌더링 */}
-                {isLoggedIn ? <LoggedInView /> : <LoggedOutView />}
+                {isLoggedIn 
+                    ? <LoggedInView userInfo={userInfo} handleLogout={handleLogout} />
+                    : <LoggedOutView openLoginModal={openLoginModal} openSignupModal={openSignupModal} />}
             </div>
         </header>
     );
