08b0d0e45204eda888d4bf2ce083f129b10cd804
개그튼코딩 <hoon62261662@gmail.com>
2025-12-14 01:10:52 +0900

    Update GalleryPage.jsx
    갤러리 코드

--- frontend/src/front/GalleryPage.jsx
+++ frontend/src/front/GalleryPage.jsx
@@ -2,6 +2,13 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getGallery } from "../api/public";
 
+export default function GalleryPage() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    getGallery().then(setImages).catch(() => setImages([]));
+  }, []);
+
   return (
     <div className="page-container fade-in">
       <h2 className="page-title">활동 채팅방</h2>
@@ -18,7 +25,6 @@ import { getGallery } from "../api/public";
               <img src={img.url} alt={img.caption} />
 
               {/* ⬇ CSS에 맞춘 오버레이 + 캡션 구조 */}
-      <h2 className="page-title">활동 채팅방
               <div className="gallery-item-overlay">
                 <div className="gallery-item-caption">
                   {img.caption}
